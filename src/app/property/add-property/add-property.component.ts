e42b9f443302a70adf39e23bbe07891b09bcaa8f
chandusamineni <saminenichandu0305@gmail.com>
2025-02-23 19:59:04 -0600

    6 commit

--- src/app/property/add-property/add-property.component.ts
+++ src/app/property/add-property/add-property.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-add-property',
@@ -6,6 +8,18 @@ import { Component } from '@angular/core';
   templateUrl: './add-property.component.html',
   styleUrl: './add-property.component.css'
 })
-export class AddPropertyComponent {
+export class AddPropertyComponent implements OnInit{
+
+  constructor(private router: Router){}
+
+  ngOnInit(): void {
+    
+  }
+  onBack(){
+   this.router.navigate(['/']);
+  
+    
+  }
+
 
 }
--- src/app/property/property-card/property-card.component.ts
+++ src/app/property/property-card/property-card.component.ts
@@ -1,9 +1,10 @@
 import { Component, Input } from '@angular/core';
 import { IProperty } from '../IProperty.interface';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-property-card',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './property-card.component.html',
   styleUrl: './property-card.component.css'
 })
